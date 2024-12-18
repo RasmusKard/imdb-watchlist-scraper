427b644e4cd07a45dfffb9cf99bc8bad2462c85b
RasmusKard <rasmuskard@gmail.com>
2024-12-18 13:44:47 +0200

    add: handle privated lists change: error logic and timeout logic

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ class WatchlistScraper {
 		userId: string;
 		timeoutInMs?: number;
 		headless?: boolean;
-		userAgent: string;
+		userAgent?: string;
 	}) {
 		this.userId = this.userIdSanitizer(userId);
 		this.idArr = [];
@@ -41,8 +41,12 @@ class WatchlistScraper {
 		});
 		const page = await context.newPage();
 
+		const imdbMetricsUrlRegex = /.*imdb\.com\/api\/_ajax\/metrics.*/;
 		await page.route("**/*", (route, request) => {
 			if (["image", "media", "stylesheet"].includes(request.resourceType())) {
+				route.abort();
+			} else if (imdbMetricsUrlRegex.test(request.url())) {
+				// abort if tries to send request to imdb metrics
 				route.abort();
 			} else {
 				route.continue();
@@ -86,16 +90,17 @@ class WatchlistScraper {
 						return;
 					}
 
-					// Scroll to last element to load
 					const lastElementIndex = this.idArr.length;
 					const lastElementLocator = this.currentPage.getByRole("link", {
 						name: new RegExp(`^${lastElementIndex}\..*`),
 					});
 
+					// navigate to the end of the page while the last element of currently rendered content isn't visible
+					// once it is scroll it into center of view
 					while (!(await lastElementLocator.isVisible())) {
 						await this.currentPage.keyboard.press("End");
-						await lastElementLocator.scrollIntoViewIfNeeded();
 					}
+					await lastElementLocator.scrollIntoViewIfNeeded();
 
 					return;
 				} catch (error) {
@@ -105,17 +110,13 @@ class WatchlistScraper {
 		});
 	}
 
-	timeoutPromise() {
-		return new Promise<void>((resolve, reject) => {
-			setTimeout(() => {
-				reject(
-					"Timed out while waiting for first POST request or while scrolling to next batch of IDs. (Timeout length may need to be increased)"
-				);
-			}, this.timeoutInMs);
-		});
-	}
-
 	async watchlistGrabIds() {
+		const timeoutID = setTimeout(() => {
+			throw new Error(
+				"Timed out while waiting for first POST request or while scrolling to next batch of IDs. (Timeout length may need to be increased)"
+			);
+		}, this.timeoutInMs);
+
 		await this.openBrowserAndBlankPage();
 
 		const postRequestListenerPromise = this.pageListenToGraphQlRequests();
@@ -123,25 +124,30 @@ class WatchlistScraper {
 			`https://www.imdb.com/user/${this.userId}/ratings/`
 		);
 
+		// Handle privated list
+		const privateTextA = this.currentPage.getByText("Private list");
+		const privateTextB = this.currentPage.getByText(
+			"The creator of this list has"
+		);
+		if ((await privateTextA.isVisible()) && (await privateTextB.isVisible())) {
+			this.closeScraper();
+			throw new Error("Watchlist is set to private");
+		}
+
 		const usernameLocator = this.currentPage.getByTestId("list-author-link");
 		this.username = (await usernameLocator.isVisible())
 			? await usernameLocator.innerText()
 			: null;
 
-		try {
-			await Promise.race([postRequestListenerPromise, this.timeoutPromise()]);
-		} catch (error) {
-			console.error(error);
-			this.closeScraper();
-			return null;
-		}
+		await postRequestListenerPromise;
+		clearTimeout(timeoutID);
 
 		this.closeScraper();
+
 		if (Array.isArray(this.idArr) && this.idArr.length) {
 			return { idArr: this.idArr, username: this.username };
 		} else {
-			console.error("No IDs found, try again");
-			return null;
+			throw new Error("Unknown error ocurred: no IDs found");
 		}
 	}
 }
